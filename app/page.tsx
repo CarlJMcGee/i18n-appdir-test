8343f5f61c1f8a4919834cf1ee0de719d6ee92f4
CarlJMcGee <98133059+CarlJMcGee@users.noreply.github.com>
2022-11-12 20:06:23 -0600

    basic scafolding

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+export default function Page() {
+  return (
+    <h1>
+      If i18n options are present in next.config.js, then this should not appear
+      on screen.
+    </h1>
+  );
+}
--- next.config.js
+++ next.config.js
@@ -2,6 +2,13 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  i18n: {
+    locales: ["en"],
+    defaultLocale: "en",
+  },
+  experimental: {
+    appDir: true,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
