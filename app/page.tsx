cd0d3ce1f781fcb3502e2f75b71778726a6093cc
CarlJMcGee <98133059+CarlJMcGee@users.noreply.github.com>
2022-11-12 20:12:27 -0600

    adjusted text

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,8 @@
 export default function Page() {
   return (
     <h1>
-      If i18n options are present in next.config.js, then this should not appear
-      on screen.
+      The i18n options are removed from next.config.js, therfore, this should be
+      visible.
     </h1>
   );
 }
